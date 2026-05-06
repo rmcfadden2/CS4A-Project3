58b034a106ef56b99474d0254fe3d93b836962d7
Mei Li Saucedo <maysaucedo849@gmail.com>
2026-05-06 14:52:52 -0700

    Magazine Created

---
+++ Magazine.java
@@ -0,0 +1,11 @@
+// public class Magazine extends Item
+// {
+//     // Variables
+//     private String magazineTitle;
+//     private String edition;
+//     private String mainArticle;
+//     private String publisher;
+
+//     // Methods
+
+// }
