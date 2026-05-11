43a0c25603227d71ca91435d3605cfbf42b96ec4
Mei Li Saucedo <maysaucedo849@gmail.com>
2026-05-11 11:38:31 -0700

    Magazine finished

--- Magazine.java
+++ Magazine.java
@@ -1,9 +1,32 @@
+import java.util.Date;
+
 public class Magazine extends Item
-{
-    // Magazine(String magazineTitle, String publisher, )
-    // {
-        
-    // }
+{   // Constructors
+
+
+    // Parameters: Item + Magazine
+    public Magazine(String name, String description, String id, String checkedPerson, Date dueDate,  int lastShelf, int lastCompartment, String magazineTitle,
+                    String edition,
+                    String mainArticle,
+                    String publisher)
+    {
+        // Checked book
+        super(name, description, id, checkedPerson, dueDate, lastShelf, lastCompartment);
+        this.magazineTitle = magazineTitle;
+        this.edition = edition;
+        this.mainArticle = mainArticle;
+        this.publisher = publisher;
+    }
+
+    // No arg - defualt constructor
+    public Magazine()
+    {
+        super();
+        magazineTitle = "Unknown Title";
+        edition = "Unknown Edition";
+        mainArticle = "Unknown Article";
+        publisher = "Unknown Publisher";
+    }
 
     // Variables
     private String magazineTitle;
@@ -47,8 +70,25 @@ public class Magazine extends Item
         this.mainArticle = mainArticle;
     }
 
-    public void getPublisher(String publisher)
+    public void setPublisher(String publisher)
     {
         this.publisher = publisher;
+    }
+
+    @Override
+    public String toString()
+    {
+        /* Reflect Item's data
+           field, while also adding its own unique 
+           magazine data field. 
+           Wanted to learn what StringBuilder does/is */
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());     // Add item info
+        sb.append("Title: ").append(magazineTitle).append("\n");         // Append - adds to the end
+        sb.append("Edition: ").append(edition).append("\n");
+        sb.append("Main Article: ").append(mainArticle).append("\n");
+        sb.append("Publisher: ").append(publisher).append("\n");
+        
+        return sb.toString();   
     }
 }
