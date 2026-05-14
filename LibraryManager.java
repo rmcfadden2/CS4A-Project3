d0957fdfed0e1a3f520644e32f6ef5bfdff01d83
Masonman101 <165975990+Masonman101@users.noreply.github.com>
2026-05-14 14:40:25 -0700

    Update LibraryManager.java
    Added functionality for adding, checking in and out items.
    
    fixed swap to update shelf/ compartment position

--- LibraryManager.java
+++ LibraryManager.java
@@ -5,23 +5,99 @@ public class LibraryManager {
     private final int NUM_COLS = 15;
     private Item[][]  libraryInventory = new Item[NUM_ROWS][NUM_COLS];
 
-    public LibraryManager()
+
+public LibraryManager()
+{
+    for (int row = 0; row < libraryInventory.length; row++)
     {
-        for (int row = 0; row < libraryInventory.length; row++) {
-        for (int col = 0; col < libraryInventory[row].length; col++) {
-            String number = String.valueOf(1000000000L + (long)(Math.random() * 9000000000L));
-            // LINE 14 HAS AN ERROR: "Item is abstract, cannot be instantiated"
-            libraryInventory[row][col] = new Item("Empty", "Item", number, row, col); // create object
+        for (int col = 0; col < libraryInventory[row].length; col++)
+        {
+            libraryInventory[row][col] = new Books(
+                "Empty",
+                "Empty",
+                "Empty",
+                0,
+                0,
+                "Empty",
+                "Empty",
+                "Empty",
+                "Empty",
+                "Empty",
+                0,
+                0,
+                "Empty"
+            );
+        }
+    }
+
+
+}
+
+    public boolean addItem(Item newItem)
+    {
+        for(int row = 0; row < libraryInventory.length; row++)
+        {
+            for(int col = 0; col < libraryInventory[row].length; col++)
+            {
+                if(libraryInventory[row][col].getName().equals("Empty"))
+                {
+                    libraryInventory[row][col] = newItem;
+                    libraryInventory[row][col].setLocation(row,col);
+                    return true;
+                }
             }
         }
-
-        libraryInventory[1][1].setName("Robert");
-        libraryInventory[1][2].setName("Robert");
-        libraryInventory[2][14].setName("Jensen");
-        libraryInventory[0][3].setName("Drew");
-        libraryInventory[0][3].checkItem("Barry");
-
+        return false;
     }
+
+    public boolean checkOutItem(String id, String person)
+{
+    for(int row = 0; row < libraryInventory.length; row++)
+    {
+        for(int col = 0; col < libraryInventory[row].length; col++)
+        {
+            Item item = libraryInventory[row][col];
+
+            if(item.getID().equals(id))
+            {
+                if(item.isChecked())
+                {
+                    return false;
+                }
+
+                item.checkItem(person);
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+public boolean checkInItem(String id)
+{
+    for(int row = 0; row < libraryInventory.length; row++)
+    {
+        for(int col = 0; col < libraryInventory[row].length; col++)
+        {
+            Item item = libraryInventory[row][col];
+
+            if(item.getID().equals(id))
+            {
+                if(!item.isChecked())
+                {
+                    return false;
+                }
+
+                item.setChecked(false);
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
     public void printItemsInStorage()
     {
@@ -74,8 +150,13 @@ public class LibraryManager {
         else
         {
             Item temp = libraryInventory[row1][col1];
+            
             libraryInventory[row1][col1] = libraryInventory[row2][col2];
+            libraryInventory[row1][col1].setLocation(row1,col1);
+            
             libraryInventory[row2][col2] = temp;
+            libraryInventory[row2][col2].setLocation(row2,col2);
+
             return 1;
         }
 
